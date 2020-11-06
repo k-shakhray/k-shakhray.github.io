be4bbf81fa25a63737c3e54168ffea43eb12fb22
Klim <shakhray3@gmail.com>
2020-11-06 17:10:07 +0200

    Updates

---
+++ precache-manifest.3fceb9abc7736bedfaf2681f0361f7a7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80e0f551047aa8e6a494ec6a3d7e136f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0dd60132420b47392a00",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "aa3c5c0188ceb2c52250",
+    "url": "/static/css/main.2961f55d.chunk.css"
+  },
+  {
+    "revision": "0dd60132420b47392a00",
+    "url": "/static/js/2.3b1b011c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.3b1b011c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa3c5c0188ceb2c52250",
+    "url": "/static/js/main.93b31167.chunk.js"
+  },
+  {
+    "revision": "b2fc5f8e1b412562a7c0",
+    "url": "/static/js/runtime-main.4b661961.js"
+  },
+  {
+    "revision": "6cd14bbf380ff62185cf1d74543cf011",
+    "url": "/static/media/alnar_school_img.6cd14bbf.jpg"
+  },
+  {
+    "revision": "00d4b3d1809fcc97e75ae668e1341869",
+    "url": "/static/media/bg_img.00d4b3d1.jpg"
+  },
+  {
+    "revision": "749348adbcad04ff848978f3a8152838",
+    "url": "/static/media/dan_it_img.749348ad.JPG"
+  },
+  {
+    "revision": "f81b374e57a0824cca860cba2a52a983",
+    "url": "/static/media/resume_photo.f81b374e.jpg"
+  }
+]);
