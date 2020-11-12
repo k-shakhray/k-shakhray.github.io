3319e5ebd747504c6bbcf5ed4997ffbbc858a9f1
Klim <shakhray3@gmail.com>
2020-11-12 19:08:23 +0200

    some improves

--- src/Components/Experience/Experience.js
+++ src/Components/Experience/Experience.js
@@ -2,7 +2,8 @@ import React from "react";
 import './Experience.css'
 import alnarIMG from '../../img/alnar_school_img.jpg'
 import danIMG from '../../img/dan_it_img.JPG'
-    
+import forkioIMG from '../../img/layout-example.jpg'
+import hamIMG from '../../img/ham-layout.jpg'
 
 export default function Experience() {
     return ( 
@@ -27,7 +28,7 @@ export default function Experience() {
                         <h3>
                             Site web-support
                         </h3>
-                        <span>Sep 2020 - Oct 2020</span>
+                        <span>Aug 2020 - Oct 2020</span>
                     </div>
                     <p>
                         Administering and maintaining pages with WordPress and ADM.tools
@@ -51,12 +52,62 @@ export default function Experience() {
                         <h3>
                             Front-end course at DAN.IT
                         </h3>
-                        <span>Sep 2019 - now</span>
+                        <span>Sep 2019 - Oct 2020</span>
                     </div>
                     <p>
                         Studied front-end developer course at <a href='https://dan-it.com.ua/' rel="noopener noreferrer"> DAN.IT Education. </a>
                     </p>
                     <a target='_blank' href='https://dan-it.com.ua/' rel="noopener noreferrer">Go to website</a>
+                </div>
+            </div>
+
+
+
+            <div className='experience-card'>
+                <div className='experience-card-illustration'>
+                    <a target='_blank' href='https://k-shakhray-portfolio.github.io/k-shakhray-portfolio/step_project_forkio/dist/index.html' rel="noopener noreferrer">
+                        <img alt='layout example, site' src={forkioIMG}/>
+                        <span>
+                            Go to website
+                        </span>
+                    </a>
+                </div>
+                <div className='experience-card-description'>
+                    <div className='experience-card-title'>
+                        <h3>
+                            Fork App
+                        </h3>
+                        <span>Example</span>
+                    </div>
+                    <p>
+                        Just an example page. Was done according to the provided layout in Photoshop
+                    </p>
+                    <a target='_blank' href='https://k-shakhray-portfolio.github.io/k-shakhray-portfolio/step_project_forkio/dist/index.html' rel="noopener noreferrer">Go to website</a>
+                </div>
+            </div>
+
+
+
+            <div className='experience-card'>
+                <div className='experience-card-illustration'>
+                    <a target='_blank' href='https://k-shakhray-portfolio.github.io/k-shakhray-portfolio/step_project_ham/index.html' rel="noopener noreferrer">
+                        <img alt='layout example, site' src={hamIMG}/>
+                        <span>
+                            Go to website
+                        </span>
+                    </a>
+                </div>
+                <div className='experience-card-description'>
+                    <div className='experience-card-title'>
+                        <h3>
+                            The Ham
+                        </h3>
+                        <span>Example</span>
+                    </div>
+                    <p>
+                        Just an example page. Was done according to the provided layout in Photoshop
+                    </p>
+                    <a target='_blank' href='https://k-shakhray-portfolio.github.io/k-shakhray-portfolio/step_project_ham/index.html' rel="noopener noreferrer">Go to website</a>
                 </div>
             </div>
 
